2f16c029601699be11094ca67e21957c7fb4b777
Pawel Filipkowski <pawebf1@gmail.com>
2022-05-20 14:09:26 +0200

    save form in db

--- app/src/Controller/CargoController.php
+++ app/src/Controller/CargoController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\Cargo;
 use App\Entity\Transport;
 use App\Form\Type\CargoType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +15,39 @@ use Symfony\Component\Routing\Annotation\Route;
 class CargoController extends AbstractController
 {
     #[Route('/', name: 'app_cargo')]
-    public function index(Request $request): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $transport = new Transport();
         $cargo = new Cargo();
+        
         $cargoForm = $this->createForm(CargoType::class, [$transport, $cargo]);
+        $cargoForm->handleRequest($request);
+
+        if ($cargoForm->isSubmitted() && $cargoForm->isValid()) {
+            $entityManager = $doctrine->getManager();
+
+            $transport->setTransportFrom($cargoForm->get('transportFrom')->getData());
+            $transport->setTransportTo($cargoForm->get('transportTo')->getData());
+            $transport->setPlane($cargoForm->get('plane')->getData());
+            $transport->setDate($cargoForm->get('date')->getData());
+
+            $filesString = '';
+            foreach ($request->files->get("cargo")['document'] as $document) {
+                $filesString .= $document->getClientOriginalName() . ' ';
+            }
+            $transport->setDocuments($filesString);
+
+            $entityManager->persist($transport);
+            $entityManager->flush();
+
+
+            $cargo->setName($cargoForm->get('transportFrom')->getData());
+            $cargo->setWeight((double)$cargoForm->get('weight')->getData());
+            $cargo->setType($cargoForm->get('type')->getData());
+            $cargo->setTransportID($transport);
+            $entityManager->persist($cargo);
+            $entityManager->flush();
+        }
 
         return $this->render('cargo/index.html.twig', [
             'controller_name' => 'CargoController',
--- app/src/Form/Type/CargoType.php
+++ app/src/Form/Type/CargoType.php
@@ -4,26 +4,66 @@ declare(strict_types=1);
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 
 class CargoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('transportFrom', TextType::class)
-            ->add('transportTo', TextType::class)
-            ->add('plane', TextType::class)
-            ->add('documents', TextType::class, [
-                'required' => false,
+            ->add('transportFrom', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter city'
+                ]
             ])
-            ->add('date', DateType::class)
-            ->add('name', TextType::class)
-            ->add('weight', TextType::class)
-            ->add('type', TextType::class)
-            ->add('submit', SubmitType::class);
+            ->add('transportTo', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter city'
+                ]
+            ])
+            ->add('plane', ChoiceType::class, [
+                'choices' => [
+                    'Airbus A380 (max weight for one cargo 35 tons)' => 'Airbus A380',
+                    'Boeing 747 (max weight for one cargo 38 tons)' => 'Boeing 747',
+                ],
+            ])
+            ->add('document', FileType::class, [
+                'required' => false,
+                'multiple' => true,
+
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'data' => new \DateTime(),
+
+            ])
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter cargo name'
+                ]
+            ])
+            ->add('weight', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter cargo weight'
+                ]
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Normal cargo' => 'Normal cargo',
+                    'Dangerous cargo' => 'Dangerous cargo',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-outline-primary'
+                ]
+            ]);
     }
 }
