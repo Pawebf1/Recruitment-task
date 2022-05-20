cbb68685a1642529044629a1f8c04da53d578477
Pawel Filipkowski <pawebf1@gmail.com>
2022-05-20 18:38:53 +0200

    create TransportType form and CargoType form

---
+++ app/src/Form/CargoType.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\Cargo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class CargoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter cargo name'
+                ]
+            ])
+            ->add('weight', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter cargo weight'
+                ]
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Normal cargo' => 'Normal cargo',
+                    'Dangerous cargo' => 'Dangerous cargo',
+                ],
+            ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cargo::class,
+        ]);
+    }
+}
--- app/src/Form/TransportType.php
+++ app/src/Form/TransportType.php
@@ -3,40 +3,36 @@ declare(strict_types=1);
 
 namespace App\Form\Type;
 
+use App\Entity\Transport;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
-class CargoType extends AbstractType
+class TransportType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('transportFrom', TextType::class, [
-                'attr' => [
-                    'placeholder' => 'Enter city'
-                ]
-            ])
-            ->add('transportTo', TextType::class, [
-                'attr' => [
-                    'placeholder' => 'Enter city'
-                ]
-            ])
+            ->add('transportFrom')
+            ->add('transportTo')
             ->add('plane', ChoiceType::class, [
                 'choices' => [
                     'Airbus A380 (max weight for one cargo 35 tons)' => 'Airbus A380',
                     'Boeing 747 (max weight for one cargo 38 tons)' => 'Boeing 747',
                 ],
             ])
-            ->add('document', FileType::class, [
+            ->add('documents', FileType::class, [
                 'required' => false,
                 'multiple' => true,
+                'mapped' => false,
 
             ])
             ->add('date', DateType::class, [
@@ -44,26 +40,25 @@ class CargoType extends AbstractType
                 'data' => new \DateTime(),
 
             ])
-            ->add('name', TextType::class, [
-                'attr' => [
-                    'placeholder' => 'Enter cargo name'
-                ]
-            ])
-            ->add('weight', TextType::class, [
-                'attr' => [
-                    'placeholder' => 'Enter cargo weight'
-                ]
-            ])
-            ->add('type', ChoiceType::class, [
-                'choices' => [
-                    'Normal cargo' => 'Normal cargo',
-                    'Dangerous cargo' => 'Dangerous cargo',
-                ],
+            ->add('cargos', CollectionType::class, [
+                'entry_type' => CargoType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-outline-primary'
                 ]
             ]);
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Transport::class,
+        ]);
     }
 }
